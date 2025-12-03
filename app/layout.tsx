4433b3c73bc034515dab93acb4efed559f459fd2
sejin <vision20400@naver.com>
2025-12-03 09:30:44 +0900

    ADD vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-
+        <Analytics />
         {/* ChannelTalk */}
         <Script
           id="channel-talk"
